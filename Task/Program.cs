cb6ddd915ad688cb1ff0ae556f21406aed7e4bc5
Alesger <Alasgarma@code.edu.az>
2022-04-06 02:27:56 +0400

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace HWHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Large(1, 2, 3);
+            //Large(1, 2, 3, 4, 5);
+        }
+        static int Large(int a, int b, int c)
+        {
+            #region Task1
+            //try
+            //{
+            //    Console.WriteLine(" Birinci ededi daxil edin");
+            //    a = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(" Ikinci ededi daxil edin");
+            //    b = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(" Uçuncu ededi daxil edin");
+            //    c = int.Parse(Console.ReadLine());
+
+            //    if (a > b && a > c)
+            //    {
+            //        Console.WriteLine($"En boyuk eded A-dir = {a}");
+
+            //    }
+            //    else if (b > a && b > c)
+            //    {
+
+            //        Console.WriteLine($"En boyuk eded B-dir = {b}");
+            //    }
+            //    else if (c > a && c > b)
+            //    {
+            //        Console.WriteLine($"En boyuk eded C-dir = {c}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Beraberlik Var");
+            //    }
+                
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine(" Duzgun Eded Daxil Edin");
+
+            //}
+            //return 0;
+            #endregion
+        }
+        static double Large(double c, double v, double b, double n, double m)
+        {
+            #region Task1
+            //try
+            //{
+            //    Console.Write("1-ci Ededi Daxil Edin:"); double f = double.Parse(Console.ReadLine());
+            //    Console.Write("2-ci Ededi Daxil Edin:"); double g = double.Parse(Console.ReadLine());
+            //    Console.Write("3-ci Ededi Daxil Edin:"); double h = double.Parse(Console.ReadLine());
+            //    Console.Write("4-ci Ededi Daxil Edin:"); double x = double.Parse(Console.ReadLine());
+            //    Console.Write("5-ci Ededi Daxil Edin:"); double j = double.Parse(Console.ReadLine());
+
+            //    double max;
+            //    if (f > g && f > h && f > x && f > j)
+            //    {
+            //        max = f;
+            //        Console.WriteLine("En Boyuk Reqemler {0} , {1} ve {2} en boyuk {3}.", f, g, h, max);
+            //    }
+            //    else if (g > f && g > h && g > x && g > j)
+            //    {
+            //        max = g;
+            //        Console.WriteLine("En Boyuk Reqemler {0} , {1} ve {2} en boyuk {3}.", f, g, h, max);
+            //    }
+            //    else if (h > f && h > g && h > x && h > j)
+            //    {
+            //        max = h;
+            //        Console.WriteLine("En Boyuk Reqemler {0} , {1} ve {2} en boyuk {3}.", f, g, h, max);
+            //    }
+            //    else if (x > f && x > g && x > h && x > j)
+            //    {
+            //        max = x;
+            //        Console.WriteLine("En Boyuk Reqemler {0} , {1} ve {2} en boyuk {3}.", f, g, h, max);
+            //    }
+            //    else
+            //    {
+            //        max = j;
+            //        Console.WriteLine("En Boyuk Reqemler {0} , {1} ve {2} en boyuk {3}.", f, g, h, max);
+            //    }
+
+
+
+                
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine(" Duzgun Eded Daxil Edin");
+
+            //}
+            //return 0;
+            #endregion
+        }
+
+    }    
+}
